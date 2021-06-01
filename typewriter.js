3971e898b088d97959041ef3f3cb3bd63586c501
Cam MacDougall <cammacdougall12@gmail.com>
2021-06-01 17:27:57 +0000

    linted typewriter.js

--- typewriter.js
+++ typewriter.js
@@ -6,7 +6,7 @@ for (const char of sentence) {
   setTimeout(() => {
     index++;
     index === sentence.length ? process.stdout.write(char + `\n`) : process.stdout.write(char);
-  }, i)
+  }, i);
   i += 100;
 }
 
