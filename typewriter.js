536d3267fd41a57836b94c55b3ef2f6525bff898
Cam MacDougall <cammacdougall12@gmail.com>
2021-06-01 17:19:58 +0000

    added typewriter.js

---
+++ typewriter.js
@@ -0,0 +1,12 @@
+const sentence = "hello there from lighthouse labs";
+let i = 0;
+let index = 0;
+
+for (const char of sentence) {
+  setTimeout(() => {
+    index++;
+    index === sentence.length ? process.stdout.write(char + `\n`) : process.stdout.write(char);
+  }, i)
+  i += 100;
+}
+
